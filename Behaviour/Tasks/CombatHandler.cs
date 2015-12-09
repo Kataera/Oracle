Kataera/Oracle

commit 5b900de82b964fb31504086b0437c092eb6eabae

Attempted bug fix
Sometimes mobs lose their FATE id after their FATE ends. This may
hopefully stop any null reference exceptions that could occur.

Behaviour/Tasks/CombatHandler.cs
@@ -67,9 +67,12 @@ namespace Tarot.Behaviour.Tasks
 
         private static bool LevelSyncNeeded()
         {
-            return Poi.Current != null && Poi.Current.Type == PoiType.Kill && Poi.Current.BattleCharacter.FateId != 0
-                   && FateManager.GetFateById(Poi.Current.BattleCharacter.FateId).IsValid
-                   && (FateManager.GetFateById(Poi.Current.BattleCharacter.FateId).MaxLevel < Core.Player.ClassLevel)
+            var fateId = Poi.Current.BattleCharacter.FateId;
+            var fate = FateManager.GetFateById(fateId);
+
+            return Poi.Current != null && Poi.Current.Type == PoiType.Kill && fateId != 0
+                   && fate.IsValid
+                   && (fate.MaxLevel < Core.Player.ClassLevel)
                    && !Core.Player.IsLevelSynced;
         }
 

Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 
-[assembly: AssemblyVersion("0.1.0.111")]
-[assembly: AssemblyFileVersion("0.1.0.111")]
+[assembly: AssemblyVersion("0.1.0.112")]
+[assembly: AssemblyFileVersion("0.1.0.112")]
 [assembly: NeutralResourcesLanguage("en-GB")]

commit 484471d4913f5e789e857dd612a46edff03dab4f

Bug fix
Fix an issue caused by commit 5b900de82b964fb31504086b0437c092eb6eabae

Behaviour/Tasks/CombatHandler.cs
@@ -67,6 +67,11 @@ namespace Tarot.Behaviour.Tasks
 
         private static bool LevelSyncNeeded()
         {
+            if (Poi.Current == null)
+            {
+                return false;
+            }
+
             var fateId = Poi.Current.BattleCharacter.FateId;
             var fate = FateManager.GetFateById(fateId);
 

Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 
-[assembly: AssemblyVersion("0.1.0.114")]
-[assembly: AssemblyFileVersion("0.1.0.114")]
+[assembly: AssemblyVersion("0.1.0.115")]
+[assembly: AssemblyFileVersion("0.1.0.115")]
 [assembly: NeutralResourcesLanguage("en-GB")]