234d8fbf9bda44ccd75576ddee04505bce5356bc
Kataera <caitlin@caitlinhowarth.co.uk>
2015-12-11 21:25:00 +0000

    Add missing FATE chain

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 
-[assembly: AssemblyVersion("0.1.0.171")]
-[assembly: AssemblyFileVersion("0.1.0.171")]
+[assembly: AssemblyVersion("0.1.0.172")]
+[assembly: AssemblyFileVersion("0.1.0.172")]
 [assembly: NeutralResourcesLanguage("en-GB")]
