ef4921f03110fc7f79db95fe7bbc360a6b6bbad9
Kataera <caitlin@caitlinhowarth.co.uk>
2015-12-12 19:57:41 +0000

    Run Like a Flame NPC id is correct
    Removing comment reminder.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 
-[assembly: AssemblyVersion("0.1.0.182")]
-[assembly: AssemblyFileVersion("0.1.0.182")]
+[assembly: AssemblyVersion("0.1.0.183")]
+[assembly: AssemblyFileVersion("0.1.0.183")]
 [assembly: NeutralResourcesLanguage("en-GB")]
