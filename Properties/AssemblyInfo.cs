d051214336d19b5e747b767d830d147121fec7b7
Kataera <caitlin@caitlinhowarth.co.uk>
2015-12-14 10:01:35 +0000

    Add cooldown for checking for targets in grind mode
    Otherwise we can get a situation where none of the targets are navigable
    and it keeps spamming the navigation server with requests.
    
    Also don't keep blacklisting a mob if it's already in the blacklist.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 
-[assembly: AssemblyVersion("0.1.0.228")]
-[assembly: AssemblyFileVersion("0.1.0.228")]
+[assembly: AssemblyVersion("0.1.0.229")]
+[assembly: AssemblyFileVersion("0.1.0.229")]
 [assembly: NeutralResourcesLanguage("en-GB")]
