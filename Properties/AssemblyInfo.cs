c2ad315c21f1da11c944bd0ad7d01d041d5e70bf
Kataera <caitlin@caitlinhowarth.co.uk>
2015-12-17 16:40:07 +0000

    Implement user FATE blacklist

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 
-[assembly: AssemblyVersion("0.1.0.290")]
-[assembly: AssemblyFileVersion("0.1.0.290")]
+[assembly: AssemblyVersion("0.1.0.291")]
+[assembly: AssemblyFileVersion("0.1.0.291")]
 [assembly: NeutralResourcesLanguage("en-GB")]
