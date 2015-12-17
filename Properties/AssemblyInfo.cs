90e3467518087da949cefff55adfb205280e4dc6
Kataera <caitlin@caitlinhowarth.co.uk>
2015-12-17 15:47:14 +0000

    Clear up debug log for closest FATE selection

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 
-[assembly: AssemblyVersion("0.1.0.285")]
-[assembly: AssemblyFileVersion("0.1.0.285")]
+[assembly: AssemblyVersion("0.1.0.286")]
+[assembly: AssemblyFileVersion("0.1.0.286")]
 [assembly: NeutralResourcesLanguage("en-GB")]
