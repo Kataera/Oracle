891f2a778742992d13717dbe3e2db67a960ceb6c
Kataera <caitlin@caitlinhowarth.co.uk>
2015-12-18 21:16:11 +0000

    Add additional checks to see if we're being attacked

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 
-[assembly: AssemblyVersion("0.1.0.329")]
-[assembly: AssemblyFileVersion("0.1.0.329")]
+[assembly: AssemblyVersion("0.1.0.330")]
+[assembly: AssemblyFileVersion("0.1.0.330")]
 [assembly: NeutralResourcesLanguage("en-GB")]
