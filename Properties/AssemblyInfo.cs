3a287cad19a62fa8cfc4a002c1ec51341db30c32
Kataera <caitlin@caitlinhowarth.co.uk>
2015-12-18 01:33:25 +0000

    Update .gitignore

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 
-[assembly: AssemblyVersion("0.1.0.313")]
-[assembly: AssemblyFileVersion("0.1.0.313")]
+[assembly: AssemblyVersion("0.1.0.314")]
+[assembly: AssemblyFileVersion("0.1.0.314")]
 [assembly: NeutralResourcesLanguage("en-GB")]
