e869258a71a4910b4d279e1e02b7459996de53ce
Kataera <caitlin@caitlinhowarth.co.uk>
2015-12-18 17:47:43 +0000

    Fix attacker check
    Since we're now force refreshing the game object cache every time the
    main worker task is called, the Poi BattleCharacter will be a different
    object to the one contained in the Attackers list.
    
    To solve this, we should check the object ID equality rather than the
    object itself.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 
-[assembly: AssemblyVersion("0.1.0.323")]
-[assembly: AssemblyFileVersion("0.1.0.323")]
+[assembly: AssemblyVersion("0.1.0.324")]
+[assembly: AssemblyFileVersion("0.1.0.324")]
 [assembly: NeutralResourcesLanguage("en-GB")]
