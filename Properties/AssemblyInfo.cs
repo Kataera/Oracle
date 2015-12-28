b74d9e212a27bb4cdcb68bc42c946c4492e4e67c
Kataera <caitlin@caitlinhowarth.co.uk>
2015-12-28 18:34:18 +0000

    Fix missing resources

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 
-[assembly: AssemblyVersion("0.1.0.395")]
-[assembly: AssemblyFileVersion("0.1.0.395")]
+[assembly: AssemblyVersion("0.1.0.396")]
+[assembly: AssemblyFileVersion("0.1.0.396")]
 [assembly: NeutralResourcesLanguage("en-GB")]
