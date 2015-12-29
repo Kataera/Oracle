a861e0d1797f91b4595afc580194f7bbd3f811d9
Kataera <caitlin@caitlinhowarth.co.uk>
2015-12-29 01:00:09 +0000

    Fix shadow clipping with edge of image

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 
-[assembly: AssemblyVersion("0.1.0.404")]
-[assembly: AssemblyFileVersion("0.1.0.404")]
+[assembly: AssemblyVersion("0.1.0.405")]
+[assembly: AssemblyFileVersion("0.1.0.405")]
 [assembly: NeutralResourcesLanguage("en-GB")]
