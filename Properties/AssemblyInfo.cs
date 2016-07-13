fd3479148b708d949697663f5f452e769c36d5fb
Caitlin <caitlin@caitlinhowarth.co.uk>
2016-07-13 03:56:01 +0100

    Begin WPF UI implementation.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 
-[assembly: AssemblyVersion("0.1.0.463")]
-[assembly: AssemblyFileVersion("0.1.0.463")]
+[assembly: AssemblyVersion("0.1.0.464")]
+[assembly: AssemblyFileVersion("0.1.0.464")]
 [assembly: NeutralResourcesLanguage("en-GB")]
--- UI/Settings.xaml.cs
+++ UI/Settings.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+using MaterialDesignThemes.Wpf;
 
 namespace Oracle.UI
 {
@@ -10,6 +14,51 @@ namespace Oracle.UI
         public Settings()
         {
             InitializeComponent();
+        }
+
+        private void CloseWindowButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void ContentPanel_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void NavigationPanel_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void MaximiseWindowButton_Click(object sender, RoutedEventArgs e)
+        {
+            switch (WindowState)
+            {
+                case WindowState.Normal:
+                    MaximiseWindowIcon.Kind = PackIconKind.WindowRestore;
+                    WindowState = WindowState.Maximized;
+                    break;
+                case WindowState.Maximized:
+                    MaximiseWindowIcon.Kind = PackIconKind.WindowMaximize;
+                    WindowState = WindowState.Normal;
+                    break;
+                case WindowState.Minimized:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private void MinimiseWindowButton_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
         }
     }
 }
