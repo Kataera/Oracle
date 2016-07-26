f292bc34c645a4628ef2fe6492cad43c2e7da2f0
Caitlin <caitlin@caitlinhowarth.co.uk>
2016-07-26 18:28:45 +0100

    Add SummonMinion task stub for the Yo-kai Watch mode.
    RebornBuddy is currently down so can't add redundancy checks yet. Will do tomorrow.

---
+++ Behaviour/Tasks/Utilities/SummonMinion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Buddy.Coroutines;
+
+using ff14bot.Managers;
+
+using Oracle.Helpers;
+
+namespace Oracle.Behaviour.Tasks.Utilities
+{
+    internal static class SummonMinion
+    {
+        public static async Task<bool> Main(string minionName)
+        {
+            // TODO: Check command and minion names in different locales.
+            Logger.SendDebugLog("Summoning " + minionName + ".");
+            ChatManager.SendChat("/minion \"" + minionName + "\"");
+            await Coroutine.Sleep(TimeSpan.FromSeconds(5));
+
+            return true;
+        }
+    }
+}
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 
-[assembly: AssemblyVersion("0.1.0.483")]
-[assembly: AssemblyFileVersion("0.1.0.483")]
+[assembly: AssemblyVersion("0.1.0.484")]
+[assembly: AssemblyFileVersion("0.1.0.484")]
 [assembly: NeutralResourcesLanguage("en-GB")]
