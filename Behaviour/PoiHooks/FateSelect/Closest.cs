Kataera/Oracle

commit 38f1268ac023296effc7dc7cba5c74fea86cc82b

Add more redundancy checks for minion summoning.
Give more debug information about closest FATEs.

Behaviour/PoiHooks/FateSelect/Closest.cs
@@ -24,15 +24,29 @@ namespace Oracle.Behaviour.PoiHooks.FateSelect
             var fateDistances = await OracleFateManager.GetActiveFateDistances();
             if (OracleSettings.Instance.TeleportIfQuicker && OracleSettings.Instance.FateSelectClosestDistanceConsiderAetheryte)
             {
-                var fateDistancesFromAetherytes = new List<Dictionary<FateData, float>>();
+                Logger.SendDebugLog("Taking into account the distance to a FATE if we teleport to it.");
 
+                var combinedDistances = fateDistances;
+                var fateDistancesFromAetherytes = new List<Dictionary<FateData, float>>();
                 foreach (var aetheryte in
                     OracleFateManager.GetAetheryteIdsForZone(WorldManager.ZoneId).Where(item => WorldManager.KnownAetheryteIds.Contains(item.Item1)))
                 {
                     fateDistancesFromAetherytes.Add(await OracleFateManager.GetActiveFateDistances(aetheryte.Item2));
                 }
 
-                var combinedDistances = fateDistances;
+                // Don't teleport for FATEs above a set percentage.
+                foreach (var fateDistance in combinedDistances)
+                {
+                    if (fateDistance.Key.Progress < OracleSettings.Instance.TeleportMaxFateProgress)
+                    {
+                        continue;
+                    }
+
+                    Logger.SendDebugLog("Ignoring teleport distance for " + fateDistance.Key.Name + ", its progress (" + fateDistance.Key.Progress
+                                        + "%) exceeds " + OracleSettings.Instance.TeleportMaxFateProgress + "%.");
+                    combinedDistances.Remove(fateDistance.Key);
+                }
+
                 foreach (var aetheryteDistanceDict in fateDistancesFromAetherytes)
                 {
                     foreach (var fateDistance in aetheryteDistanceDict)
@@ -45,12 +59,6 @@ namespace Oracle.Behaviour.PoiHooks.FateSelect
 
                         // Add the minimum distance delta to ensure we don't teleport if distance saved is under that amount.
                         if (fateDistance.Value + OracleSettings.Instance.TeleportMinimumDistanceDelta >= currentDistance)
-                        {
-                            continue;
-                        }
-
-                        // Don't teleport for FATEs above a set percentage.
-                        if (fateDistance.Key.Progress >= OracleSettings.Instance.TeleportMaxFateProgress)
                         {
                             continue;
                         }

Behaviour/Tasks/Utilities/SummonMinion.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 using Buddy.Coroutines;
@@ -6,6 +7,7 @@ using Buddy.Coroutines;
 using ff14bot;
 using ff14bot.Behavior;
 using ff14bot.Managers;
+using ff14bot.Objects;
 
 using Oracle.Helpers;
 
@@ -13,6 +15,28 @@ namespace Oracle.Behaviour.Tasks.Utilities
 {
     internal static class SummonMinion
     {
+        public static GameObject CurrentMinion => GameObjectManager.GetObjectByObjectId(1);
+
+        public static bool CanSummonMinion()
+        {
+            if (Core.Player.IsMounted)
+            {
+                return false;
+            }
+
+            if (Core.Player.InCombat)
+            {
+                return false;
+            }
+
+            if (GameObjectManager.Attackers.Any())
+            {
+                return false;
+            }
+
+            return true;
+        }
+
         public static async Task<bool> IsMinionSummoned(string minionName)
         {
             if (Core.Player.IsMounted)
@@ -22,10 +46,7 @@ namespace Oracle.Behaviour.Tasks.Utilities
 
             // Ensure GameObjectManager data is fresh.
             RefreshObjectManagerCache();
-
-            // For some reason, the player's minion always has an ObjectId of 1.
-            var minionObject = GameObjectManager.GetObjectByObjectId(1);
-            return minionObject != null && minionObject.EnglishName.Equals(minionName);
+            return CurrentMinion != null && CurrentMinion.EnglishName.Equals(minionName);
         }
 
         public static async Task<SummonMinionResult> Main(string minionName)
@@ -37,24 +58,22 @@ namespace Oracle.Behaviour.Tasks.Utilities
                 await CommonTasks.StopAndDismount();
             }
 
-            if (GameObjectManager.GetObjectByObjectId(1) != null)
+            if (CurrentMinion != null)
             {
                 ChatManager.SendChat("/minion");
-                await Coroutine.Wait(TimeSpan.FromSeconds(5), () => GameObjectManager.GetObjectByObjectId(1) == null);
+                await Coroutine.Wait(TimeSpan.FromSeconds(5), () => CurrentMinion == null);
             }
 
             // Only way to summon minion with current RebornBuddy API is through a chat command.
             ChatManager.SendChat("/minion \"" + minionName + "\"");
-            await Coroutine.Wait(TimeSpan.FromSeconds(5), () => GameObjectManager.GetObjectByObjectId(1) != null);
+            await Coroutine.Wait(TimeSpan.FromSeconds(5), () => CurrentMinion != null);
 
             // Ensure GameObjectManager data is fresh.
             RefreshObjectManagerCache();
 
-            // For some reason, the player's minion always has an ObjectId of 1.
-            var minionObject = GameObjectManager.GetObjectByObjectId(1);
-            if (minionObject != null)
+            if (CurrentMinion != null)
             {
-                if (minionObject.EnglishName.Equals(minionName))
+                if (CurrentMinion.EnglishName.Equals(minionName))
                 {
                     Logger.SendLog(minionName + " has been summoned successfully!");
                     return SummonMinionResult.Success;

Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 
-[assembly: AssemblyVersion("0.1.0.499")]
-[assembly: AssemblyFileVersion("0.1.0.499")]
+[assembly: AssemblyVersion("0.1.0.500")]
+[assembly: AssemblyFileVersion("0.1.0.500")]
 [assembly: NeutralResourcesLanguage("en-GB")]

Behaviour/Modes/YokaiWatchGrind.cs
@@ -151,7 +151,7 @@ namespace Oracle.Behaviour.Modes
 
             if (!ConditionParser.HasAtLeast(YokaiMinions.Jibanyan.MedalItemId, OracleSettings.Instance.YokaiJibanyanMedalsToFarm) && !ignoreJibanyan)
             {
-                if (!Core.Player.IsMounted && !Core.Player.InCombat && !await SummonMinion.IsMinionSummoned(YokaiMinions.Jibanyan.EnglishName))
+                if (SummonMinion.CanSummonMinion() && !await SummonMinion.IsMinionSummoned(YokaiMinions.Jibanyan.EnglishName))
                 {
                     var summonMinionResult = await SummonMinion.Main(YokaiMinions.Jibanyan.EnglishName);
                     if (summonMinionResult != SummonMinionResult.Success)
@@ -164,7 +164,7 @@ namespace Oracle.Behaviour.Modes
             }
             else if (!ConditionParser.HasAtLeast(YokaiMinions.Komasan.MedalItemId, OracleSettings.Instance.YokaiKomasanMedalsToFarm) && !ignoreKomasan)
             {
-                if (!Core.Player.IsMounted && !Core.Player.InCombat && !await SummonMinion.IsMinionSummoned(YokaiMinions.Komasan.EnglishName))
+                if (SummonMinion.CanSummonMinion() && !await SummonMinion.IsMinionSummoned(YokaiMinions.Komasan.EnglishName))
                 {
                     var summonMinionResult = await SummonMinion.Main(YokaiMinions.Komasan.EnglishName);
                     if (summonMinionResult != SummonMinionResult.Success)
@@ -177,7 +177,7 @@ namespace Oracle.Behaviour.Modes
             }
             else if (!ConditionParser.HasAtLeast(YokaiMinions.Usapyon.MedalItemId, OracleSettings.Instance.YokaiUsapyonMedalsToFarm) && !ignoreUsapyon)
             {
-                if (!Core.Player.IsMounted && !Core.Player.InCombat && !await SummonMinion.IsMinionSummoned(YokaiMinions.Usapyon.EnglishName))
+                if (SummonMinion.CanSummonMinion() && !await SummonMinion.IsMinionSummoned(YokaiMinions.Usapyon.EnglishName))
                 {
                     var summonMinionResult = await SummonMinion.Main(YokaiMinions.Usapyon.EnglishName);
                     if (summonMinionResult != SummonMinionResult.Success)
@@ -190,7 +190,7 @@ namespace Oracle.Behaviour.Modes
             }
             else if (!ConditionParser.HasAtLeast(YokaiMinions.Whisper.MedalItemId, OracleSettings.Instance.YokaiWhisperMedalsToFarm) && !ignoreWhisper)
             {
-                if (!Core.Player.IsMounted && !Core.Player.InCombat && !await SummonMinion.IsMinionSummoned(YokaiMinions.Whisper.EnglishName))
+                if (SummonMinion.CanSummonMinion() && !await SummonMinion.IsMinionSummoned(YokaiMinions.Whisper.EnglishName))
                 {
                     var summonMinionResult = await SummonMinion.Main(YokaiMinions.Whisper.EnglishName);
                     if (summonMinionResult != SummonMinionResult.Success)
@@ -203,7 +203,7 @@ namespace Oracle.Behaviour.Modes
             }
             else if (!ConditionParser.HasAtLeast(YokaiMinions.Shogunyan.MedalItemId, OracleSettings.Instance.YokaiShogunyanMedalsToFarm) && !ignoreShogunyan)
             {
-                if (!Core.Player.IsMounted && !Core.Player.InCombat && !await SummonMinion.IsMinionSummoned(YokaiMinions.Shogunyan.EnglishName))
+                if (SummonMinion.CanSummonMinion() && !await SummonMinion.IsMinionSummoned(YokaiMinions.Shogunyan.EnglishName))
                 {
                     var summonMinionResult = await SummonMinion.Main(YokaiMinions.Shogunyan.EnglishName);
                     if (summonMinionResult != SummonMinionResult.Success)
@@ -216,7 +216,7 @@ namespace Oracle.Behaviour.Modes
             }
             else if (!ConditionParser.HasAtLeast(YokaiMinions.Hovernyan.MedalItemId, OracleSettings.Instance.YokaiHovernyanMedalsToFarm) && !ignoreHovernyan)
             {
-                if (!Core.Player.IsMounted && !Core.Player.InCombat && !await SummonMinion.IsMinionSummoned(YokaiMinions.Hovernyan.EnglishName))
+                if (SummonMinion.CanSummonMinion() && !await SummonMinion.IsMinionSummoned(YokaiMinions.Hovernyan.EnglishName))
                 {
                     var summonMinionResult = await SummonMinion.Main(YokaiMinions.Hovernyan.EnglishName);
                     if (summonMinionResult != SummonMinionResult.Success)
@@ -229,7 +229,7 @@ namespace Oracle.Behaviour.Modes
             }
             else if (!ConditionParser.HasAtLeast(YokaiMinions.Komajiro.MedalItemId, OracleSettings.Instance.YokaiKomajiroMedalsToFarm) && !ignoreKomajiro)
             {
-                if (!Core.Player.IsMounted && !Core.Player.InCombat && !await SummonMinion.IsMinionSummoned(YokaiMinions.Komajiro.EnglishName))
+                if (SummonMinion.CanSummonMinion() && !await SummonMinion.IsMinionSummoned(YokaiMinions.Komajiro.EnglishName))
                 {
                     var summonMinionResult = await SummonMinion.Main(YokaiMinions.Komajiro.EnglishName);
                     if (summonMinionResult != SummonMinionResult.Success)
@@ -242,7 +242,7 @@ namespace Oracle.Behaviour.Modes
             }
             else if (!ConditionParser.HasAtLeast(YokaiMinions.Noko.MedalItemId, OracleSettings.Instance.YokaiNokoMedalsToFarm) && !ignoreNoko)
             {
-                if (!Core.Player.IsMounted && !Core.Player.InCombat && !await SummonMinion.IsMinionSummoned(YokaiMinions.Noko.EnglishName))
+                if (SummonMinion.CanSummonMinion() && !await SummonMinion.IsMinionSummoned(YokaiMinions.Noko.EnglishName))
                 {
                     var summonMinionResult = await SummonMinion.Main(YokaiMinions.Noko.EnglishName);
                     if (summonMinionResult != SummonMinionResult.Success)
@@ -255,7 +255,7 @@ namespace Oracle.Behaviour.Modes
             }
             else if (!ConditionParser.HasAtLeast(YokaiMinions.Venoct.MedalItemId, OracleSettings.Instance.YokaiVenoctMedalsToFarm) && !ignoreVenoct)
             {
-                if (!Core.Player.IsMounted && !Core.Player.InCombat && !await SummonMinion.IsMinionSummoned(YokaiMinions.Venoct.EnglishName))
+                if (SummonMinion.CanSummonMinion() && !await SummonMinion.IsMinionSummoned(YokaiMinions.Venoct.EnglishName))
                 {
                     var summonMinionResult = await SummonMinion.Main(YokaiMinions.Venoct.EnglishName);
                     if (summonMinionResult != SummonMinionResult.Success)
@@ -268,7 +268,7 @@ namespace Oracle.Behaviour.Modes
             }
             else if (!ConditionParser.HasAtLeast(YokaiMinions.Kyubi.MedalItemId, OracleSettings.Instance.YokaiKyubiMedalsToFarm) && !ignoreKyubi)
             {
-                if (!Core.Player.IsMounted && !Core.Player.InCombat && !await SummonMinion.IsMinionSummoned(YokaiMinions.Kyubi.EnglishName))
+                if (SummonMinion.CanSummonMinion() && !await SummonMinion.IsMinionSummoned(YokaiMinions.Kyubi.EnglishName))
                 {
                     var summonMinionResult = await SummonMinion.Main(YokaiMinions.Kyubi.EnglishName);
                     if (summonMinionResult != SummonMinionResult.Success)
@@ -281,7 +281,7 @@ namespace Oracle.Behaviour.Modes
             }
             else if (!ConditionParser.HasAtLeast(YokaiMinions.Robonyan.MedalItemId, OracleSettings.Instance.YokaiRobonyanMedalsToFarm) && !ignoreRobonyan)
             {
-                if (!Core.Player.IsMounted && !Core.Player.InCombat && !await SummonMinion.IsMinionSummoned(YokaiMinions.Robonyan.EnglishName))
+                if (SummonMinion.CanSummonMinion() && !await SummonMinion.IsMinionSummoned(YokaiMinions.Robonyan.EnglishName))
                 {
                     var summonMinionResult = await SummonMinion.Main(YokaiMinions.Robonyan.EnglishName);
                     if (summonMinionResult != SummonMinionResult.Success)
@@ -294,7 +294,7 @@ namespace Oracle.Behaviour.Modes
             }
             else if (!ConditionParser.HasAtLeast(YokaiMinions.Blizzaria.MedalItemId, OracleSettings.Instance.YokaiBlizzariaMedalsToFarm) && !ignoreBlizzaria)
             {
-                if (!Core.Player.IsMounted && !Core.Player.InCombat && !await SummonMinion.IsMinionSummoned(YokaiMinions.Blizzaria.EnglishName))
+                if (SummonMinion.CanSummonMinion() && !await SummonMinion.IsMinionSummoned(YokaiMinions.Blizzaria.EnglishName))
                 {
                     var summonMinionResult = await SummonMinion.Main(YokaiMinions.Blizzaria.EnglishName);
                     if (summonMinionResult != SummonMinionResult.Success)
@@ -307,7 +307,7 @@ namespace Oracle.Behaviour.Modes
             }
             else if (!ConditionParser.HasAtLeast(YokaiMinions.Manjimutt.MedalItemId, OracleSettings.Instance.YokaiManjimuttMedalsToFarm) && !ignoreManjimutt)
             {
-                if (!Core.Player.IsMounted && !Core.Player.InCombat && !await SummonMinion.IsMinionSummoned(YokaiMinions.Manjimutt.EnglishName))
+                if (SummonMinion.CanSummonMinion() && !await SummonMinion.IsMinionSummoned(YokaiMinions.Manjimutt.EnglishName))
                 {
                     var summonMinionResult = await SummonMinion.Main(YokaiMinions.Manjimutt.EnglishName);
                     if (summonMinionResult != SummonMinionResult.Success)

commit 0cd18ef24a525d43132f9b0705b434078dfd4c23

Fix enumeration issue introduced in 38f1268ac023296effc7dc7cba5c74fea86cc82b.

Behaviour/PoiHooks/FateSelect/Closest.cs
@@ -35,7 +35,7 @@ namespace Oracle.Behaviour.PoiHooks.FateSelect
                 }
 
                 // Don't teleport for FATEs above a set percentage.
-                foreach (var fateDistance in combinedDistances)
+                foreach (var fateDistance in await OracleFateManager.GetActiveFateDistances())
                 {
                     if (fateDistance.Key.Progress < OracleSettings.Instance.TeleportMaxFateProgress)
                     {

Behaviour/Tasks/Utilities/SummonMinion.cs
@@ -44,7 +44,6 @@ namespace Oracle.Behaviour.Tasks.Utilities
                 return true;
             }
 
-            // Ensure GameObjectManager data is fresh.
             RefreshObjectManagerCache();
             return CurrentMinion != null && CurrentMinion.EnglishName.Equals(minionName);
         }
@@ -68,9 +67,7 @@ namespace Oracle.Behaviour.Tasks.Utilities
             ChatManager.SendChat("/minion \"" + minionName + "\"");
             await Coroutine.Wait(TimeSpan.FromSeconds(5), () => CurrentMinion != null);
 
-            // Ensure GameObjectManager data is fresh.
             RefreshObjectManagerCache();
-
             if (CurrentMinion != null)
             {
                 if (CurrentMinion.EnglishName.Equals(minionName))

Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 
-[assembly: AssemblyVersion("0.1.0.500")]
-[assembly: AssemblyFileVersion("0.1.0.500")]
+[assembly: AssemblyVersion("0.1.0.501")]
+[assembly: AssemblyFileVersion("0.1.0.501")]
 [assembly: NeutralResourcesLanguage("en-GB")]