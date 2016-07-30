500f1434133022c78f03bfb184b954fed2a46ead
Caitlin <caitlin@caitlinhowarth.co.uk>
2016-07-30 01:02:13 +0100

    Fix another logic fail.
    Shouldn't code when sleepy. :(

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 
-[assembly: AssemblyVersion("0.1.0.507")]
-[assembly: AssemblyFileVersion("0.1.0.507")]
+[assembly: AssemblyVersion("0.1.0.509")]
+[assembly: AssemblyFileVersion("0.1.0.509")]
 [assembly: NeutralResourcesLanguage("en-GB")]
