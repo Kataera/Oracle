2bb81963bdb882d698823d6e7b042371039784a4
Caitlin <caitlin@caitlinhowarth.co.uk>
2016-08-21 21:49:51 +0100

    Set Rutting Buffalo as a preferred target for the FATE Just a Matter of Rut.
    Fix a log about selecting a target to use format the object id as a hex string.

--- Managers/OracleCombatManager.cs
+++ Managers/OracleCombatManager.cs
@@ -57,7 +57,7 @@ namespace Oracle.Managers
 
             if (target != null)
             {
-                Logger.SendLog("Selected " + target.Name + " (" + target.ObjectId + ") as the next target.");
+                Logger.SendLog("Selected " + target.Name + " (" + target.ObjectId.ToString("X") + ") as the next target.");
                 Poi.Current = new Poi(target, PoiType.Kill);
             }
         }
